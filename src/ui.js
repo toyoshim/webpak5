dcb139e980268b0f30181a747495612487b29605
Takashi Toyoshima <toyoshim@gmail.com>
2013-10-06 01:12:30 +0900

    style fix

--- src/ui.js
+++ src/ui.js
@@ -85,19 +85,19 @@ $(function () {
         var file = e.target.files[0];
         var reader = new FileReader();
         reader.onload = function () {
-$('#progress').parent().addClass('progress-striped');
-$('#progress').parent().addClass('active');
-$('#progress').css({'width': '100%'});
-loading = true;
-player.load(this.result, function () {
-    loading = false;
-    $('#progress').parent().removeClass('progress-striped');
-    $('#progress').parent().removeClass('active');
-    $('#progress').css({'width': '0%'});
-    lastDuration = -1;
-    drawMarkers();
-    animation();
-});
+            $('#progress').parent().addClass('progress-striped');
+            $('#progress').parent().addClass('active');
+            $('#progress').css({'width': '100%'});
+            loading = true;
+            player.load(this.result, function () {
+                loading = false;
+                $('#progress').parent().removeClass('progress-striped');
+                $('#progress').parent().removeClass('active');
+                $('#progress').css({'width': '0%'});
+                lastDuration = -1;
+                drawMarkers();
+                animation();
+            });
         };
         reader.readAsArrayBuffer(file);
     });
@@ -109,7 +109,7 @@ player.load(this.result, function () {
     });
     $('#progress-marker').on('mousemove', function (e) {
         if (!markerDrag)
-return;
+            return;
         handleMarkerDrag(e);
     });
     $('#progress-marker').on('mouseup', function (e) {
@@ -121,34 +121,34 @@ return;
     // Set button click handler.
     $('.btn').on('click', function () {
         var handlers = {
-'source-stereo': function () { player.setSource(Mp3Player.SOURCE_STEREO); },
-'source-l+r': function () { player.setSource(Mp3Player.SOURCE_L_PLUS_R); },
-'source-l': function () { player.setSource(Mp3Player.SOURCE_L); },
-'source-r': function () { player.setSource(Mp3Player.SOURCE_R); },
-'source-l-r': function () { player.setSource(Mp3Player.SOURCE_L_MINUS_R); },
+            'source-stereo': function () { player.setSource(Mp3Player.SOURCE_STEREO); },
+            'source-l+r': function () { player.setSource(Mp3Player.SOURCE_L_PLUS_R); },
+            'source-l': function () { player.setSource(Mp3Player.SOURCE_L); },
+            'source-r': function () { player.setSource(Mp3Player.SOURCE_R); },
+            'source-l-r': function () { player.setSource(Mp3Player.SOURCE_L_MINUS_R); },
 
-'filter-flat': function () {},
-'filter-1': function () { log('not implemented.'); },
-'filter-2': function () { log('not implemented.'); },
-'filter-3': function () { log('not implemented.'); },
-'filter-4': function () { log('not implemented.'); },
+            'filter-flat': function () {},
+            'filter-1': function () { log('not implemented.'); },
+            'filter-2': function () { log('not implemented.'); },
+            'filter-3': function () { log('not implemented.'); },
+            'filter-4': function () { log('not implemented.'); },
 
-'pitch-x2': function () { pitch = 2; player.setSpeed(speed, pitch); },
-'pitch-x1': function () { pitch = 1; player.setSpeed(speed, pitch); },
-'pitch-x0.5': function ()  { pitch = 0.5; player.setSpeed(speed, pitch); },
+            'pitch-x2': function () { pitch = 2; player.setSpeed(speed, pitch); },
+            'pitch-x1': function () { pitch = 1; player.setSpeed(speed, pitch); },
+            'pitch-x0.5': function ()  { pitch = 0.5; player.setSpeed(speed, pitch); },
 
-'speed-x2': function () { speed = 2; player.setSpeed(speed, pitch); },
-'speed-x1': function () { speed = 1; player.setSpeed(speed, pitch); },
-'speed-x0.5': function () { speed = 0.5; player.setSpeed(speed, pitch); },
+            'speed-x2': function () { speed = 2; player.setSpeed(speed, pitch); },
+            'speed-x1': function () { speed = 1; player.setSpeed(speed, pitch); },
+            'speed-x0.5': function () { speed = 0.5; player.setSpeed(speed, pitch); },
 
-'button-eject': fileOpenDialog,
-'button-stepback': player.stepback.bind(player),
-'button-stop': player.stop.bind(player),
-'button-play': player.play.bind(player),
-'button-pause': function () { player.pause(!player.isPaused()); },
-'button-stepforward': player.stepforward.bind(player),
-'button-mark-start': function () { player.markAsStart(); drawMarkers(); },
-'button-mark-end': function () { player.markAsEnd(); drawMarkers(); }
+            'button-eject': fileOpenDialog,
+            'button-stepback': player.stepback.bind(player),
+            'button-stop': player.stop.bind(player),
+            'button-play': player.play.bind(player),
+            'button-pause': function () { player.pause(!player.isPaused()); },
+            'button-stepforward': player.stepforward.bind(player),
+            'button-mark-start': function () { player.markAsStart(); drawMarkers(); },
+            'button-mark-end': function () { player.markAsEnd(); drawMarkers(); }
         };
         var id = $(this).attr('id');
         if (!handlers[id])
@@ -157,51 +157,51 @@ return;
     });
     $('.btn').on('mousedown', function () {
         var handlers = {
-'button-back': function (q) { player.setSpeed(speed * -8, speed * 8); },
-'button-forward': function (q) { player.setSpeed(speed * 8, speed * 8); },
+            'button-back': function (q) { player.setSpeed(speed * -8, speed * 8); },
+            'button-forward': function (q) { player.setSpeed(speed * 8, speed * 8); },
         };
         var id = $(this).attr('id');
         if (!handlers[id])
-return;
+            return;
         handlers[id]();
     });
     $('.btn').on('mouseup', function () {
         var handlers = {
-'button-back': function () { player.setSpeed(speed, pitch); },
-'button-forward': function () { player.setSpeed(speed, pitch); },
+            'button-back': function () { player.setSpeed(speed, pitch); },
+            'button-forward': function () { player.setSpeed(speed, pitch); },
         };
         var id = $(this).attr('id');
         if (!handlers[id])
-return;
+            return;
         handlers[id]();
     });
     $('body').on('keydown', function (e) {
         var handlers = {
-'E': [ fileOpenDialog, null ],
-'Z': [ function (q) { player.setSpeed(speed * -8, speed * 8); }, player.stepback.bind(player) ],
-'S': [ player.stop.bind(player), null ],
-' ': [ player.play.bind(player), null ],
-'X': [ function (q) { player.pause(!player.isPaused()); }, null ],
-'C': [ function (q) { player.setSpeed(speed * 8, speed * 8); }, player.stepforward.bind(player) ],
-'N': [ function (q) { player.markAsStart(); drawMarkers(); }, null ],
-'M': [ function (q) { player.markAsEnd(); drawMarkers(); }, null ]
+            'E': [ fileOpenDialog, null ],
+            'Z': [ function (q) { player.setSpeed(speed * -8, speed * 8); }, player.stepback.bind(player) ],
+            'S': [ player.stop.bind(player), null ],
+            ' ': [ player.play.bind(player), null ],
+            'X': [ function (q) { player.pause(!player.isPaused()); }, null ],
+            'C': [ function (q) { player.setSpeed(speed * 8, speed * 8); }, player.stepforward.bind(player) ],
+            'N': [ function (q) { player.markAsStart(); drawMarkers(); }, null ],
+            'M': [ function (q) { player.markAsEnd(); drawMarkers(); }, null ]
         }
         var char = String.fromCharCode(e.keyCode);
         if (!handlers[char])
-return;
+            return;
         var index = e.shiftKey ? 1 : 0;
         if (!handlers[char][index])
-return;
+            return;
         handlers[char][index]();
     });
     $('body').on('keyup', function (e) {
         var handlers = {
-'Z': function () { player.setSpeed(speed, pitch); },
-'C': function () { player.setSpeed(speed, pitch); }
+            'Z': function () { player.setSpeed(speed, pitch); },
+            'C': function () { player.setSpeed(speed, pitch); }
         }
         var char = String.fromCharCode(e.keyCode);
         if (!handlers[char])
-return;
+            return;
         handlers[char]();
     });
 });
