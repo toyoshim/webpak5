79463d2ac1424acab5709e8216519e28c907e21d
Takashi Toyoshima <toyoshim@gmail.com>
2015-03-20 19:18:22 +0000

    Add missing launch.js file

---
+++ chrome/launch.js
@@ -0,0 +1,11 @@
+chrome.app.runtime.onLaunched.addListener(function(data) {
+  chrome.app.window.create('ui.html', {
+    resizable: false,
+    bounds: {
+      'width': 480,
+      'height': 460 
+    }
+  }, function (window) {
+    window.contentWindow._openFile = data.items[0];
+  }.bind(data));
+});
